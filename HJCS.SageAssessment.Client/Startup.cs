aeaf183fdbf1f3395ecf061768f42fa57e6d5c78
Javier Castillo <javier.castillosuazo@gmail.com>
2017-05-25 21:07:10 +0200

    4th iteration of Create New Invoice (add Invariant Model Binder Provider for deimal type)

--- HJCS.SageAssessment.Client/Startup.cs
+++ HJCS.SageAssessment.Client/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Localization;
 using System.Globalization;
+using HJCS.SageAssessment.ClientMVC.AppCode;
 
 namespace HJCS.SageAssessment.ClientMVC
 {
@@ -25,15 +26,12 @@ namespace HJCS.SageAssessment.ClientMVC
         }
 
         public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc();
+            services.AddMvc(config => config.ModelBinderProviders.Insert(0, new InvariantDecimalModelBinderProvider()));
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
